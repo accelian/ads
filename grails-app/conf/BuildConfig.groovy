1a1330cc5602d2ee9b9edab5dd2aba37b6a93725
MSloan <msloan21@gmail.com>
2015-07-02 10:47:45 -0400

    cleaning up

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -65,10 +65,6 @@ grails.project.dependency.resolution = {
 		compile ":twitter-bootstrap:3.3.4"
 		compile ":spring-mobile:1.1.3"
 		compile ":d3:3.4.8.0"
-		
-		
-		
-		
 
         // plugins needed at runtime but not for compilation
         runtime ":jquery:1.11.1"
